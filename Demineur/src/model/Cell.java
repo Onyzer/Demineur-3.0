6589fa434301be86882ad730ae29be0475016766
onyze <onyze@172.18.1.238>
2017-03-08 11:57:26 +0100

    Implémentation de la classe Cell.java

--- Demineur/src/model/Cell.java
+++ Demineur/src/model/Cell.java
@@ -10,5 +10,43 @@ package model;
  * @author onyze
  */
 public class Cell {
+    private int risk, x, y;
+    private boolean hidden, marked;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.risk = 0;
+        this.hidden = true;
+        this.marked = false;
+    }
+
+    public int getRisk() {
+        return risk;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public boolean isHidden() {
+        return hidden;
+    }
+    
+    public void addRisk(){
+        this.risk += 1;
+    }
+
+    public void setHidden(boolean hidden) {
+        this.hidden = hidden;
+    }
+
+    public void setMarked(boolean marked) {
+        this.marked = marked;
+    }
     
 }
