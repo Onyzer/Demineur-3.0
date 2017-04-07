0eb6c2cef4b65854017aa63499b37ef996106e42
Fertin Milan <onyzer59190@gmail.com>
2017-04-07 17:01:54 +0200

    Update Demineur.java

--- Demineur/src/Demineur/Demineur.java
+++ Demineur/src/Demineur/Demineur.java
@@ -15,27 +15,7 @@ import java.util.Scanner;
  */
 public class Demineur {
     public static void main(String[] args){
-        int xSize, ySize, bombs = 0;
-        String action;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Bienvenue dans le super Démineur !");
-        //Récupération de la taille en X
-        System.out.println("Quelle sera le nombre de cellules en X ?");
-        xSize = Integer.parseInt(sc.nextLine());
-        System.out.println("Quelle sera le nombre de cellules en Y ?");
-        ySize = Integer.parseInt(sc.nextLine());
-        System.out.println("Quelle sera le pourcentage de bombes dans la grille ?");
-        while (bombs > 85 || bombs <= 0) {
-            try {
-                bombs = Integer.parseInt(sc.nextLine());
-                if (bombs > 85 || bombs <= 0) {
-                    throw new BombsException();
-                }
-            } catch (BombsException e) {
-            }
-
-        }
-        Game game = new Game(xSize, ySize, bombs);
+        Game game = new Game();
         game.playGraphical();
     }
 }
