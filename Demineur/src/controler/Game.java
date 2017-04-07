c174a6652671898e4d3dbec4babfdb20ebb19e39
Fertin Milan <onyzer59190@gmail.com>
2017-04-07 17:00:42 +0200

    Update Game.java

--- Demineur/src/controler/Game.java
+++ Demineur/src/controler/Game.java
@@ -5,28 +5,43 @@
  */
 package controler;
 
+import java.awt.PopupMenu;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.util.Scanner;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JProgressBar;
+import javax.swing.JSlider;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
 import model.Grid;
+import view.CustomGameFrame;
 import view.GraphicalGridView;
+import view.MenuBar;
 import view.consoleView;
 
 /**
  *
  * @author onyze
  */
-public class Game{
+public class Game {
+
+    private int x = 9;
+    private int y = 9;
+    private int bombs = 10;
     private Grid grille;
-    
-    public Game(int xSize, int ySize, int bombs) {
-        this.grille = new Grid(xSize, ySize, bombs);
+    private GraphicalGridView graph;
+
+    public Game() {
+
     }
-    
-    public void playConsole(){
+
+    /*public void playConsole(){
         consoleView cons = new consoleView(grille);
         grille.addObserver(cons);
         Scanner sc = new Scanner(System.in);
@@ -62,16 +77,72 @@ public class Game{
                     break;
             }
         }
-    }
-    
+    }*/
     public void playGraphical() {
 
         JFrame frame = new JFrame("Démineur");
-        GraphicalGridView graph = new GraphicalGridView(grille);
+
+        MenuBar menuBar = new MenuBar();
+        frame.setJMenuBar(menuBar);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(500, 500);
+        frame.setSize(300, 400);
+        grille = new Grid(x, y, bombs);
+        graph = new GraphicalGridView(grille);
         frame.add(graph);
         grille.addObserver(graph);
         frame.setVisible(true);
+        menuBar.getCustom().addActionListener((ActionEvent ev) -> {
+            CustomGameFrame cgf = new CustomGameFrame();
+            x = cgf.getRows();
+            y = cgf.getColumns();
+            bombs = cgf.getMines();
+            grille = new Grid(x, y, bombs);
+            frame.remove(graph);
+            graph = new GraphicalGridView(grille);
+            frame.add(graph);
+            grille.addObserver(graph);
+            frame.repaint();
+            frame.setVisible(true);
+        });
+
+        menuBar.getBeginner().addActionListener((ActionEvent ev) -> {
+            x = 9;
+            y = 9;
+            bombs = 10;
+            grille = new Grid(x, y, bombs);
+            frame.remove(graph);
+            graph = new GraphicalGridView(grille);
+            frame.add(graph);
+            grille.addObserver(graph);
+            frame.repaint();
+            frame.setVisible(true);
+        });
+
+        menuBar.getIntermediate().addActionListener((ActionEvent ev) -> {
+            x = 16;
+            y = 16;
+            bombs = 40;
+            grille = new Grid(x, y, bombs);
+            frame.remove(graph);
+            graph = new GraphicalGridView(grille);
+            frame.add(graph);
+            grille.addObserver(graph);
+            frame.repaint();
+            frame.setVisible(true);
+        });
+
+        menuBar.getExpert().addActionListener((ActionEvent ev) -> {
+            x = 16;
+            y = 30;
+            bombs = 99;
+            grille = new Grid(x, y, bombs);
+            frame.remove(graph);
+            graph = new GraphicalGridView(grille);
+            frame.add(graph);
+            grille.addObserver(graph);
+            frame.repaint();
+            frame.setVisible(true);
+        });
+        
     }
 }
